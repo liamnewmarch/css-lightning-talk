aff642236ff07b00803b928a241913249619e29c
Liam Newmarch <liamnewmarch@users.noreply.github.com>
2024-05-28 17:51:40 +0100

    Add minimal scaffold

---
+++ index.js
@@ -0,0 +1,12 @@
+import Reveal from '/node_modules/reveal.js/dist/reveal.esm.js'
+import Markdown from '/node_modules/reveal.js/plugin/markdown/markdown.esm.js'
+import Highlight from '/node_modules/reveal.js/plugin/highlight/highlight.esm.js'
+
+const app = new Reveal({
+    plugins: [
+        Markdown,
+        Highlight,
+    ],
+})
+
+app.initialize()
