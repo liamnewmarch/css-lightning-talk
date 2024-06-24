a1f1a21eebdeee4e9731ef73c818489e2dda98b8
Liam Newmarch <liamnewmarch@users.noreply.github.com>
2024-06-24 15:21:43 +0100

    Use relative paths for resources

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-import Reveal from '/node_modules/reveal.js/dist/reveal.esm.js'
-import Markdown from '/node_modules/reveal.js/plugin/markdown/markdown.esm.js'
-import Highlight from '/node_modules/reveal.js/plugin/highlight/highlight.esm.js'
+import Reveal from './node_modules/reveal.js/dist/reveal.esm.js'
+import Markdown from './node_modules/reveal.js/plugin/markdown/markdown.esm.js'
+import Highlight from './node_modules/reveal.js/plugin/highlight/highlight.esm.js'
 
 const app = new Reveal({
     plugins: [
