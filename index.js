d4abeaebdd3f68010fdaf447bdc8338063800d8d
Liam Newmarch <liamnewmarch@users.noreply.github.com>
2024-06-26 11:09:10 +0100

    Small tidy up

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 import Reveal from './node_modules/reveal.js/dist/reveal.esm.js'
-import Markdown from './node_modules/reveal.js/plugin/markdown/markdown.esm.js'
 import Highlight from './node_modules/reveal.js/plugin/highlight/highlight.esm.js'
 
 const app = new Reveal({
     plugins: [
-        Markdown,
         Highlight,
     ],
 })
